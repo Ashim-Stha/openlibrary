de41df0a3b58c492d8dcf75a503d420588141ff8
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-04-29 11:32:37 +0545

    test: add homepage test
    Signed-off-by: Ashim-Stha <ashimshrestha2384@gmail.com>

---
+++ test/pages/HomePage.ts
@@ -0,0 +1,30 @@
+import { Locator, Page } from "@playwright/test";
+import { home_url } from "../helper-config";
+
+export class HomePage {
+  page: Page;
+  loginLinkLocator: Locator;
+  signupLinkLocator: Locator;
+  welcomeLocator: Locator;
+
+  constructor({ page }: { page: Page }) {
+    this.page = page;
+    this.loginLinkLocator = page.getByRole("link", { name: "Log In" });
+    this.signupLinkLocator = page.getByRole("link", { name: "Sign Up" });
+    this.welcomeLocator = page.getByRole("heading", {
+      name: "Welcome to Open Library",
+    });
+  }
+
+  async navigateToHome() {
+    await this.page.goto(home_url);
+  }
+
+  async goToLogin() {
+    await this.loginLinkLocator.click();
+  }
+
+  async goToSignup() {
+    await this.signupLinkLocator.click();
+  }
+}
