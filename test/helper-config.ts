bb4b71c32f2ccee8b299c789b4e797550d3aec3b
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-05-07 10:41:43 +0545

    test:add and remove author image

--- test/helper-config.ts
+++ test/helper-config.ts
@@ -4,3 +4,5 @@ export const VALID_EMAIL: string = process.env.EMAIL ?? "hello@gmail.com";
 export const VALID_PASSWORD: string = process.env.PASSWORD ?? "password";
 export const INVALID_EMAIL: string = process.env.INVALID_EMAIL ?? "invalid";
 export const SIGNUP_URL: string = process.env.SIGNUP_URL ?? " ";
+export const IMAGE_PATH: string =
+  "./images/stars.png";
